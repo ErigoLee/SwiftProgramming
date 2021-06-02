bbfdf2106e303697ffeb0167617154da26025092
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-06-03 01:24:16 +0900

    Homework12

---
+++ Homework12/DetailController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailController.swift
+//  Homework12
+//
+//  Created by mac20 on 2021/05/24.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    @IBOutlet var mealImageView: UIImageView!
+    var image:UIImage!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        mealImageView.image = image
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Homework12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework12
+//
+//  Created by mac20 on 2021/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
